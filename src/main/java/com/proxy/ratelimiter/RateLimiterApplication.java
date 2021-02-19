b20d5828f6d806d4d6307b9fe52a32dbe9d31114
Nirakh Rastogi <nir16r@gmail.com>
2021-02-20 02:06:59 +0530

    Initial Commit

--- src/main/java/com/proxy/ratelimiter/RateLimiterApplication.java
+++ src/main/java/com/proxy/ratelimiter/RateLimiterApplication.java
@@ -2,12 +2,14 @@ package com.proxy.ratelimiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RateLimiterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RateLimiterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimiterApplication.class, args);
+    }
 
 }
